c08ff0d511beb36b94c32b6a27cb32c32c412901
CodeDreaded <71469916+CodeDreaded@users.noreply.github.com>
2022-04-15 09:49:50 -0400

    Update Dreaded_Currency.sol

--- Dreaded_Currency.sol
+++ Dreaded_Currency.sol
@@ -30,12 +30,31 @@ contract DreadedCurrency {
 */
     
     // Governance Token
-    uint256 public tokenGrapes; /*<---State Variables (Notice their Scope is outside the function
+    uint256 tokenGrapes; /*<---State Variables (Notice their Scope is outside the function
 *                               below. */
 
     // Utility Token
-    uint256 public tokenSHIT = 1; // <-- Also a State Variable. Set Ratio to Grapes 1000:1
+    uint256 tokenSHIT; // <-- Also a State Variable. Set Ratio to Grapes 1000:1
 
+    //Structs
+    //Struct types can be used inside mappings and arrays.
+    //They can themselves contain mappings and arrays.
+    //https://docs.soliditylang.org/en/latest/types.html?highlight=structs#structs
+    struct DreadedApe {
+        uint256 tokenSHIT;  /* ----SHIT Coin here--- */
+        string name;
+
+    }
+    //Calling Struct [Objects]
+    //SYNTAX:Call the Struct name, Visibility, Name of Function (Button) = Struct name () <--Pass value to Struct Vars
+    // {($VAR: foo, $VAR2: "foo2"} <-- Strings($VAR2) need "". This should be in () to pass value.
+    // Call Struct then give name. Pass value to Struct
+    // DreadedApe public ape = DreadedApe({tokenSHIT: 2000, name: "King Kong"});
+
+    //Array. A list or group of Info.
+    //Syntax: ArrayType[] [Visibility] name;
+    // Call Struct
+    DreadedApe[] public Orangutans;
 
 
     // This funtion named "store", pass a value name "-_tokenGrapes".
@@ -47,4 +66,32 @@ contract DreadedCurrency {
         
     
     }
+    //State Mutability [View, Pure]
+    //https://docs.soliditylang.org/en/latest/contracts.html?highlight=Return#state-mutability
+
+    //View
+    //This function named "retrieve" is used to returns $VAR tokenGrapes.
+    //The "view" declaration ensures the function does not change the state.
+    //https://docs.soliditylang.org/en/latest/contracts.html?highlight=view#view-functions
+
+    //The returns
+    //https://docs.soliditylang.org/en/latest/contracts.html?highlight=Return#return-variables
+    function retrieve() public view returns(uint256) {
+        return tokenGrapes;
+
+    }
+
+    //Pure
+    // Just does math without saving the State.
+    //https://docs.soliditylang.org/en/latest/contracts.html?highlight=Return#pure-functions
+    
+
+
+    //Function to pass ($VAR1, $VAR2) from Orangutans Array
+    function addApe(string memory _name, uint256 _tokenSHIT) public {
+        //Orangutans are Declared at Array Call above
+        Orangutans.push(DreadedApe({tokenSHIT: _tokenSHIT, name: _name}));
+    }
+
+
 }
